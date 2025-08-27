ba41a618b43deb68dd0fc9d3ae2f6e3a48e65d5a
Avnish Patel <er.avnishpatel023@gmail.com>
2025-08-27 23:54:14 +0530

    Variables in java

---
+++ Variables and Datatypes/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Avnish Patel";
+        System.out.println(name);
+
+        a = 30;
+        System.out.println(a);
+
+        b=a;
+        System.out.println(b);
+    }
+}
