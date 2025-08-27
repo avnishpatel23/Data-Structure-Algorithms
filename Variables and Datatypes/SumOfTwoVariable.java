e837c54221ca9fee170db80aadbd693cbbc7c36b
Avnish Patel <er.avnishpatel023@gmail.com>
2025-08-28 02:36:22 +0530

    Sum Of Two Variable a and b.

---
+++ Variables and Datatypes/SumOfTwoVariable.java
@@ -0,0 +1,10 @@
+public class SumOfTwoVariable {
+    
+      // code to calculate sum
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
