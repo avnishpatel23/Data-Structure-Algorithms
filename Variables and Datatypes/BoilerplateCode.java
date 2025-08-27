e128a6231ea112b41332c27b3583044296bda879
Avnish Patel <er.avnishpatel023@gmail.com>
2025-08-27 23:33:55 +0530

    Boilerplate Code and Output in Java Programming.

--- Variables and Datatypes/BoilerplateCode.java
+++ Variables and Datatypes/BoilerplateCode.java
@@ -1,9 +1,10 @@
 public class BoilerplateCode {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        System.out.println("Hello Word!\nHello Java!");
         System.out.println("1 2 3 4 5");
     }
 }
+
 
 // Boilerplate Code
