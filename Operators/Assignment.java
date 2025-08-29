6393cdf9bcec30d876c053a67cef68a2279da75a
Avnish Patel <er.avnishpatel023@gmail.com>
2025-08-30 00:13:06 +0530

    Assignment Operator =, +=, -=, *=, /=

---
+++ Operators/Assignment.java
@@ -0,0 +1,12 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println(A=B);
+        System.out.println(A+=10);
+        System.out.println(A-=5);
+        System.out.println(A*=6);
+        System.out.println(B/=5);
+
+    }
+}
