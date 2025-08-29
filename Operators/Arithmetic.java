e90d8971d45f637619025e8ed352e9bf23e2b69f
Avnish Patel <er.avnishpatel023@gmail.com>
2025-08-30 00:11:58 +0530

    Arithmetic Operator +, -, *, /, %

---
+++ Operators/Arithmetic.java
@@ -0,0 +1,13 @@
+
+public class Arithmetic {
+    public static void main(String[] args){
+        int A = 10;
+        int B = 5;
+        System.out.println("add = " +(A+B));
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+    
+}
