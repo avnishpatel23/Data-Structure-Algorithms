975dc4cf91696d233263228ee2294130d468dec9
Avnish Patel <er.avnishpatel023@gmail.com>
2025-08-30 00:10:39 +0530

    Logical Operator &&, ||, !

---
+++ Operators/Logical.java
@@ -0,0 +1,7 @@
+public class Logical {
+    public static void main(String[] args) {
+        System.out.println((3>2) && (5<4));
+        System.out.println((3>4) || (6>5) );
+        System.out.println(!(3>2));
+    }
+}
