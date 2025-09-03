b6174bf4372388b06f7f2d8f864b86b0d9fcf035
Avnish Patel <er.avnishpatel023@gmail.com>
2025-09-03 13:54:29 +0530

    Ternary Operator

---
+++ Conditional Statements/TernaryOperator.java
@@ -0,0 +1,23 @@
+// public class TernaryOperator {
+//     public static void main(String[] args) {
+//         int num = 5;
+//         String type = ((num % 2) ==0) ? "even" : "odd" ;
+//         System.out.println(type);
+//     }
+// }
+
+
+/* Check if a student will Pass or Fail */
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        int marks = 40;
+        String status = (marks >= 33) ? "Pass" : "fail";
+        System.out.println(status);
+    }
+}
+
+
+
+
