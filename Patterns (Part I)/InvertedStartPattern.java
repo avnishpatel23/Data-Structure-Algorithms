ad92f0b134bee079f56e46c54cbd9cd862bd2a6f
Avnish Patel <er.avnishpatel023@gmail.com>
2025-09-04 13:48:14 +0530

    Print Inverted Star Pattern

---
+++ Patterns (Part I)/InvertedStartPattern.java
@@ -0,0 +1,11 @@
+public class InvertedStartPattern {
+    public static void main(String[] args) {
+        int row = 4;
+        for(int line=1; line<=row; line++){
+            for(int star=1; star<=row-line+1; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
