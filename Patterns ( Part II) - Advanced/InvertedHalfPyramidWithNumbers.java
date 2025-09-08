9b799c6eedc061857e553e3e85b70e7fab98cef1
Avnish Patel <er.avnishpatel023@gmail.com>
2025-09-08 11:52:15 +0530

    Print Inverted Half Pyramid With Numbers Pattern

---
+++ Patterns ( Part II) - Advanced/InvertedHalfPyramidWithNumbers.java
@@ -0,0 +1,14 @@
+public class InvertedHalfPyramidWithNumbers {
+    public static void inverte_half_pyramid_withNumbers (int n){
+        for (int i=1; i<=n; i++){
+            // inner numbers
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        inverte_half_pyramid_withNumbers(5);
+    }
+}
