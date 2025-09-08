ab48d04b41acd8dc2bf37f53b8367cc592d56a8f
Avnish Patel <er.avnishpatel023@gmail.com>
2025-09-08 19:52:10 +0530

    Find Length of an Arrays

---
+++ ARRAYS (Part I)/LengthOfArray.java
@@ -0,0 +1,6 @@
+public class LengthOfArray {
+    public static void main(String[] args) {
+        int marks[] = new int [100];
+        System.out.println("length if array = " +marks.length);
+    }
+}
