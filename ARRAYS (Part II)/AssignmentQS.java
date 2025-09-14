331d2b1d7f2d7b4be96604d8493f933e64bedbd1
Avnish Patel <er.avnishpatel023@gmail.com>
2025-09-14 17:33:32 +0530

    AssignmentQS

--- ARRAYS (Part II)/AssignmentQS.java
+++ ARRAYS (Part II)/AssignmentQS.java
@@ -41,47 +41,98 @@
 
 /* Ans 2 */
 
-public class AssignmentQS {
+// public class AssignmentQS {
 
-    public static int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+//     public static int search(int[] nums, int target) {
+//         int left = 0, right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
 
-            // target found
-            if (nums[mid] == target) {
-                return mid;
-            }
+//             // target found
+//             if (nums[mid] == target) {
+//                 return mid;
+//             }
 
-            // Check if left half is sorted
-            if (nums[left] <= nums[mid]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1; // search left
+//             // Check if left half is sorted
+//             if (nums[left] <= nums[mid]) {
+//                 if (target >= nums[left] && target < nums[mid]) {
+//                     right = mid - 1; // search left
+//                 } else {
+//                     left = mid + 1; // search right
+//                 }
+//             }
+//             // Otherwise right half is sorted
+//             else {
+//                 if (target > nums[mid] && target <= nums[right]) {
+//                     left = mid + 1; // search right
+//                 } else {
+//                     right = mid - 1; // search left
+//                 }
+//             }
+//         }
+
+//         return -1; // not found
+//     }
+
+//     public static void main(String[] args) {
+//         int[] nums1 = {4, 5, 6, 7, 0, 1, 2};
+//         int target1 = 0;
+//         System.out.println("Output: " + search(nums1, target1)); // 4
+
+//         int[] nums2 = {4, 5, 6, 7, 0, 1, 2};
+//         int target2 = 3;
+//         System.out.println("Output: " + search(nums2, target2)); // -1
+//     }
+// }
+
+
+
+/* Ans 3  - 3Sum*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class AssignmentQS8 {
+
+    public List<List<Integer>> threeSum(int[] nums){
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>>ans = new ArrayList<>();
+        Set<List<Integer>>s = new HashSet<>();
+
+        for(int i=0; i<n-2; i++){
+            int low = i+1;
+            int high = n-1;
+            while (low<high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum == 0){
+                    s.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    low++;
+                    high--;
+                } else if (sum<0){
+                    low++;
                 } else {
-                    left = mid + 1; // search right
-                }
-            }
-            // Otherwise right half is sorted
-            else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1; // search right
-                } else {
-                    right = mid - 1; // search left
+                    high--;
                 }
             }
         }
-
-        return -1; // not found
+        ans.addAll(s);
+        return ans;
     }
 
-    public static void main(String[] args) {
-        int[] nums1 = {4, 5, 6, 7, 0, 1, 2};
-        int target1 = 0;
-        System.out.println("Output: " + search(nums1, target1)); // 4
+     public static void main(String[] args) {
+        int[] nums = {-4, -1, -1, 0, 1, 2};
 
-        int[] nums2 = {4, 5, 6, 7, 0, 1, 2};
-        int target2 = 3;
-        System.out.println("Output: " + search(nums2, target2)); // -1
+        AssignmentQS8 obj = new AssignmentQS8(); 
+        List<List<Integer>> result = obj.threeSum(nums);
+
+        System.out.println(result);
     }
+
 }
+
+
