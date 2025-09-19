496495e730fc379b2501d5ba904d3517c01baf86
Avnish Patel <er.avnishpatel023@gmail.com>
2025-09-19 23:22:28 +0530

    Print Substring of a given string

---
+++ Strings/Substring.java
@@ -0,0 +1,18 @@
+public class Substring {
+    // public static String substring (String str, int si, int ei){
+    //     String subStr = "";
+    //     for(int i = si; i<ei; i++){
+    //         subStr += str.charAt(i);
+    //     } 
+    //     return subStr;
+    // }
+    // public static void main(String[] args) {
+    //     String str = "HelloWorld";
+    //     System.out.println(substring(str, 0, 5));
+    // }
+
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        System.out.println(str.substring(0, 4));
+    }
+}
